da3642bc69571c79dd336ce741f978fd403da0eb
ahmad-zizo <a7md_zizo@yahoo.com>
2015-08-26 17:05:17 +0200

    Add users avatars

--- MovieProject/app/models/user.rb
+++ MovieProject/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -9,7 +11,7 @@ class User < ActiveRecord::Base
   has_many :favorites, dependent: :destroy
   has_many :ratings, dependent: :destroy
 
-  
+
 
 
 end
--- MovieProject/db/schema.rb
+++ MovieProject/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824165515) do
+ActiveRecord::Schema.define(version: 20150826142602) do
 
   create_table "acmos", force: :cascade do |t|
     t.integer  "actor_id",   limit: 4
