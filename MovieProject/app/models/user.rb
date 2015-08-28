e6811ae131a7abe085bbe4718a5f50f9f0222c87
hackerghost93 <ahmedws93@hotmail.com>
2015-08-28 23:31:42 +0200

    Add username.

--- MovieProject/app/models/user.rb
+++ MovieProject/app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_attached_file :avatar, styles: { medium: "400x600#", thumb: "50x50#" }
+  has_attached_file :avatar, styles: { medium: "400x600#", thumb: "75x75#" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  validates :username , presence: true,uniqueness: true
+  
   has_many :comments, dependent: :destroy
   has_many :movies, dependent: :destroy
   has_many :favorites, dependent: :destroy
--- MovieProject/db/schema.rb
+++ MovieProject/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826142602) do
+ActiveRecord::Schema.define(version: 20150828211816) do
 
   create_table "acmos", force: :cascade do |t|
     t.integer  "actor_id",   limit: 4
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20150826142602) do
     t.string   "avatar_content_type",    limit: 255
     t.integer  "avatar_file_size",       limit: 4
     t.datetime "avatar_updated_at"
+    t.string   "username",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
